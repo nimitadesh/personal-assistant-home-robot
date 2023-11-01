636afeb8b830453f2a9e34cde925770d50bd0def
Nimita Deshpande <nimita.ahs@gmail.com>
2023-11-01 12:56:01 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.centeredContent}>
+        {/* Call Contacts */}
+        <View style={styles.buttonSection}>
+          <Button
+            title="Call Contacts"
+            onPress={() => console.log("Calling contacts...")}
+          />
+          <Text style={styles.description}>Call your friends and family</Text>
+        </View>
+
+        {/* Divider */}
+        <View style={styles.divider} />
+
+        {/* Travel */}
+        <View style={styles.buttonSection}>
+          <Button title="Travel" onPress={() => console.log("Traveling...")} />
+          <Text style={styles.description}>Travel to different places</Text>
+        </View>
+
+        {/* Divider */}
+        <View style={styles.divider} />
+
+        {/* Smart Home */}
+        <View style={styles.buttonSection}>
+          <Button
+            title="Smart Home"
+            onPress={() => console.log("Controlling smart home...")}
+          />
+          <Text style={styles.description}>
+            Control your smart home devices
+          </Text>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center", // Center vertically
+    alignItems: "center", // Center horizontally
+  },
+  centeredContent: {
+    alignItems: "center", // Center horizontally
+  },
+  buttonSection: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  description: {
+    marginTop: 8,
+    textAlign: "center",
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    marginVertical: 16,
   },
 });
+
+export default App;
